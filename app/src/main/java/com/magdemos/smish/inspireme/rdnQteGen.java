bf345fe41cc4e516a69c8a392546eed3c3c7e5a9
Sritam Mishra <s.m.temporare@gmail.com>
2015-08-06 22:06:22 -0700

    Commented

--- app/src/main/java/com/magdemos/smish/inspireme/rdnQteGen.java
+++ app/src/main/java/com/magdemos/smish/inspireme/rdnQteGen.java
@@ -1,26 +1,22 @@
 package com.magdemos.smish.inspireme;
 
 import java.util.Random;
-
-/**
- * Created by smdroidstudio.tk on 2/14/15.
- */
-
+//Imports the classes that we will need in this class
 
 public class rdnQteGen {
-    private String[] qtsList = {
+    private String[] qtsList = { //List of quotes
         "You have been inspired! Test Quote 1" +
-                "\n-John Doe#1",// this is an example of concatenation
+                "\n-John Doe#1",//This is an example of concatenation
         "You have been inspired! Test Quote 2\n-John Doe#2",
         "You have been inspired! Test Quote 3\n-John Doe#3",
         "You have been inspired! Test Quote 4\n-Jane Doe#1",
         "You have been inspired! Test Quote 5\n–John Doe#5",
         "You have been inspired! Test Quote 6\n-John Doe#6",
-        "Android is really bad!\n–Said no one ever"
+        "Android is really bad!\n–Said no one ever"//"\n" just means next line
     };
     public String rtnQte() {
-        Random rndmGen = new Random();
-        int rndmNum = rndmGen.nextInt(qtsList.length);
-        return qtsList[rndmNum];
+        Random rndmGen = new Random();//Initialize
+        int rndmNum = rndmGen.nextInt(qtsList.length);//Select random value from array
+        return qtsList[rndmNum];//Return that value (String) from the array
     }
 }
